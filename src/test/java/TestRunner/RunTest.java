be3b716f2c0a377e1ae0c51e22b10f93bd219eba
inbg1 <balamurugan_gnanam@neimanmarcus.com>
2019-09-11 16:55:07 -0500

    Add all my files

---
+++ src/test/java/TestRunner/RunTest.java
@@ -0,0 +1,18 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\inbg1\\eclipse-workspace\\com.cicd\\Features\\testsign.feature"
+		,glue = {"stepDefition"}
+		
+		)
+
+
+public class RunTest {
+
+}
